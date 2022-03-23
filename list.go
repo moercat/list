1d9b2b7be24c8e3961abdded984a14dabff272e5
moero <1410146941@qq.com>
2022-03-23 10:12:57 +0800

    set string

--- list.go
+++ list.go
@@ -34,20 +34,58 @@ func NilList(va interface{}) List {
 	}
 }
 
-func (d List) Copy() List {
-	d.ensureInitialized()
-	return List{
-		value:  &(*d.value),
-		length: d.length,
-	}
-}
-
 func NewStrSlice(va interface{}) *[]string {
 	val := cast.ToStringSlice(va)
 	return &val
 }
 
-// Abs returns the absolute value of the string slice.
+func (d List) Min() int {
+
+	d2 := *d.value
+	d3 := cast.ToIntSlice(d2)
+	sort.Ints(d3)
+
+	return d3[0]
+}
+
+func (d List) Max() int {
+
+	d2 := *d.value
+	d3 := cast.ToIntSlice(d2)
+	sort.Ints(d3)
+
+	return d3[len(d3)-1]
+}
+
+func (d List) Sum() int {
+	total := 0
+	for _, item := range *d.value {
+		total += cast.ToInt(item)
+	}
+
+	return total
+}
+
+func (d List) Set() List {
+	rdv := *d.value
+	d2Map := make(map[string]bool)
+	for _, v := range rdv {
+		if !d2Map[v] {
+			d2Map[v] = true
+		}
+	}
+
+	d3Value := make([]string, 0, d.length)
+	for k, _ := range d2Map {
+		d3Value = append(d3Value, k)
+	}
+
+	return List{
+		value:  &d3Value,
+		length: len(d3Value),
+	}
+}
+
 func (d List) Abs() List {
 	d.ensureInitialized()
 	d2Value := make([]string, 0, len(*d.value))
@@ -65,22 +103,33 @@ func (d List) Abs() List {
 	}
 }
 
-// Add returns d + d2.
-func (d List) Add(d2 List) List {
-	rdv := *d.value
-	rdv2 := *d2.value
-	l2 := d.length + d2.length
-	d3Value := append(rdv, rdv2...)
+func (d List) Copy() List {
+	d.ensureInitialized()
 	return List{
-		value:  &d3Value,
-		length: l2,
+		value:  &(*d.value),
+		length: d.length,
 	}
 }
 
-// Sub returns d - d2.
-func (d List) Sub(d2 List) List {
+func (d List) Pop(idx int) {
+	fats := *d.value
+	if idx < 0 {
+		idx = d.length + idx
+	}
+	*d.value = append(fats[:idx], fats[(idx+1):]...)
+}
+
+func (d List) Extend(sub interface{}) {
+
+	subs := cast.ToStringSlice(sub)
+	*d.value = append(*d.value, subs...)
+
+}
+
+func (d List) Dup(d2 interface{}) List {
+	dv := cast.ToStringSlice(d2)
 	rdv := *d.value
-	rdv2 := *d2.value
+	rdv2 := dv
 	d2Value := append(rdv, rdv2...)
 	d2Map := make(map[string]bool)
 	for _, v := range d2Value {
@@ -100,116 +149,9 @@ func (d List) Sub(d2 List) List {
 	}
 }
 
-// Equal returns whether the numbers represented by d and d2 are equal.
-func (d List) Equal(d2 List) bool {
-
-	s1 := *d.value
-	s2 := *d2.value
-	if len(s1) != len(s2) {
-		return false
-	}
-	for i, n := range s1 {
-		if n != s2[i] {
-			return false
-		}
-	}
-	return true
-}
-
-// Length returns the length
-func (d List) Length() int {
-	return d.length
-}
-
-// IntSlice returns IntSlice
-func (d List) IntSlice() []int {
-	d.ensureInitialized()
-	dValue := *d.value
-	return cast.ToIntSlice(dValue)
-}
-
-func (d List) BoolSlice() []bool {
-	dValue := *d.value
-	return cast.ToBoolSlice(dValue)
-}
-
-func (d List) StringSlice() []string {
-	return d.string()
-}
-
-// String 修改输出结果为数组形式
-func (d List) String() string {
-	v := *d.value
-	return "[" + strings.Join(v, " ") + "]"
-}
-
-// Value implements the driver.Valuer interface for database serialization.
-func (d List) Value() (driver.Value, error) {
-	return d.String(), nil
-}
-
-func (d List) string() []string {
-	return cast.ToStringSlice(*d.value)
-}
-
-func (d *List) ensureInitialized() {
-	if d.value == nil {
-		d.value = new([]string)
-	}
-}
-
-func (d List) Min() int {
-
-	d2 := *d.value
-	d3 := cast.ToIntSlice(d2)
-	sort.Ints(d3)
-
-	return d3[0]
-}
-
-// Max returns the largest List that was passed in the arguments.
-func (d List) Max() int {
-
-	d2 := *d.value
-	d3 := cast.ToIntSlice(d2)
-	sort.Ints(d3)
-
-	return d3[len(d3)-1]
-}
-
-// Sum returns the combined total of the provided first and rest Decimals
-func (d List) Sum() int {
-	total := 0
-	for _, item := range *d.value {
-		total += cast.ToInt(item)
-	}
-
-	return total
-}
-
 func (d List) In(sub interface{}) bool {
 	_, ok := inI(d.value, sub)
 	return ok
-}
-
-func (d List) Index(sub interface{}) int {
-	index, _ := inI(d.value, sub)
-	return index
-}
-
-func (d List) Extend(sub interface{}) {
-
-	subs := cast.ToStringSlice(sub)
-	*d.value = append(*d.value, subs...)
-
-}
-
-func (d List) Pop(idx int) {
-	fats := *d.value
-	if idx < 0 {
-		idx = d.length + idx
-	}
-	*d.value = append(fats[:idx], fats[(idx+1):]...)
 }
 
 func (d List) Remove(value interface{}) {
@@ -226,6 +168,26 @@ func (d List) Append(value interface{}) {
 	fats := *d.value
 	str := cast.ToString(value)
 	*d.value = append(fats, str)
+}
+
+func (d List) Equal(d2 interface{}) bool {
+	dv := cast.ToStringSlice(d2)
+	s1 := *d.value
+	s2 := dv
+	if d.length != len(s2) {
+		return false
+	}
+	for i, n := range s1 {
+		if n != s2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func (d List) Index(sub interface{}) int {
+	index, _ := inI(d.value, sub)
+	return index
 }
 
 func (d List) Insert(idx int, value interface{}) {
@@ -248,6 +210,46 @@ func (d List) Count(value interface{}) (count int) {
 	}
 
 	return
+}
+
+// Length returns the length
+func (d List) Length() int {
+	return d.length
+}
+
+func (d List) IntSlice() []int {
+	d.ensureInitialized()
+	dValue := *d.value
+	return cast.ToIntSlice(dValue)
+}
+
+func (d List) BoolSlice() []bool {
+	dValue := *d.value
+	return cast.ToBoolSlice(dValue)
+}
+
+func (d List) StringSlice() []string {
+	return d.string()
+}
+
+// String 修改输出结果为数组形式
+func (d List) String() string {
+	v := *d.value
+	return "[" + strings.Join(v, " ") + "]"
+}
+
+func (d List) Value() (driver.Value, error) {
+	return d.String(), nil
+}
+
+func (d List) string() []string {
+	return cast.ToStringSlice(*d.value)
+}
+
+func (d List) ensureInitialized() {
+	if d.value == nil {
+		d.value = new([]string)
+	}
 }
 
 func inI(fat *[]string, sub interface{}) (int, bool) {
